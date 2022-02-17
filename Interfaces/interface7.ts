4e77833d5bbd413394c7082c592f39cbfda9c76b
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-02-17 17:07:45 +0900

    22/02/17/05_07

---
+++ Interfaces/interface7.ts
@@ -0,0 +1,10 @@
+interface HelloPerson {
+  (name: string, age?: number): void;
+}
+// helloPerson 함수는 interface HelloPerson과 상호작용
+// 뒤에 function에  name,age를 받았는데 함수내에 age가 없음으로 error 발생
+const helloPerson: HelloPerson = function (name:string, /*age:number*/) {
+  console.log(`안녕하세요 ${name} 입니다.`)
+}
+
+helloPerson("Mark", 25)
